639f4d44a60a26efbf0072c29204a4267acc5f14
nglmercer <nglmercer@gmail.com>
2024-11-07 03:13:50 +0000

    actions list

---
+++ src/Actions.js
@@ -0,0 +1,27 @@
+const Actions = {
+    "StartStreaming": {
+        "id": 1,
+        "name": "StartStreaming",
+        "description": "Start streaming",
+        "parameters": [
+            {
+                "id": 1,
+                "name": "stream",
+                "description": "Stream to start",
+                "type": "Stream"
+            }
+        ]
+    },
+    "StopStreaming": {
+        "id": 2,
+        "name": "StopStreaming",
+        "description": "Stop streaming",
+        "parameters": [
+            {
+                "id": 1,
+                "name": "stream",
+                "description": "Stream to stop",
+                "type": "Stream"
+            }
+        ]
+    }}
